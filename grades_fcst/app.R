58cf17b0b1c585d786309238baa9e147a82482a1
pbenavidesh <pbenavides@iteso.mx>
2020-05-18 14:50:27 -0500

    final grades P2020

--- grades_fcst/app.R
+++ grades_fcst/app.R
@@ -27,7 +27,8 @@ ui <- fluidPage(
     titlePanel("Calificaciones Series de tiempo P2020"),
     tabsetPanel(
         tabPanel("Calificaciones globales",
-                 plotOutput("res_glob")
+                 plotOutput("res_glob")#,
+                 # plotOutput("res_glob_fin")
                  ),
         tabPanel("Calificaciones individuales",
                  sidebarLayout(
@@ -35,22 +36,32 @@ ui <- fluidPage(
                          numericInput(inputId = "Exp",
                                       label = "Expediente",
                                       min = 0,step = 1,
-                                      value = 0),
-                         numericInput(inputId = "Ex3",
-                                      label = "Calificación examen 3 (del 0 al 10)",
-                                      min = 0, max = 10,
-                                      step = 0.1,
-                                      value = 10),
-                         numericInput(inputId = "Proy",
-                                      label = "Calificación del proyecto final (del 0 al 10)",
-                                      min = 0, max = 10,
-                                      step = 0.1,
-                                      value = 10)
+                                      value = 0)#,
+                         # numericInput(inputId = "Ex3",
+                         #              label = "Calificación examen 3 (del 0 al 10)",
+                         #              min = 0, max = 10,
+                         #              step = 0.1,
+                         #              value = 10),
+                         # numericInput(inputId = "Proy",
+                         #              label = "Calificación del proyecto final (del 0 al 10)",
+                         #              min = 0, max = 10,
+                         #              step = 0.1,
+                         #              value = 10)
                      ),
                      
                      # Show a plot of the generated distribution
                      mainPanel(
-                         tableOutput("c_final"),
+                         splitLayout(cellWidths = c("60%", "40%"),
+                             tableOutput("c_final"),
+                             htmlOutput("c_boleta"),
+                             tags$head(tags$style("#c_boleta{color: blue;
+                                    font-size: 20px;
+                                    font-style: bold;
+                                    text-align: center;
+                                    }"
+                             )
+                             )
+                         ), 
                          tableOutput("califs")
                          
                          
@@ -68,6 +79,29 @@ server <- function(input, output) {
         df %>% 
             filter(Exp == input$Exp)
     })
+    
+    calif_previa <- reactive({
+        c_previa <- Data() %>% 
+            group_by(Grupo, Sexo, Exp, Rubro) %>%
+            summarise(Calificación = mean(Calificación))
+        examenes <- c_previa %>%
+            filter(Rubro == "Examen")
+        examenes <- examenes$Calificación
+        # examenes <- (examenes$Calificación * 2 +
+        #                  input$Ex3) / 3 # previo
+        tareas <- c_previa %>%
+            filter(Rubro == "Tarea")
+        tareas <- tareas$Calificación
+        # proyecto <- input$Proy # previo
+        proyecto <- c_previa %>% 
+            filter(Rubro == "Proyecto")
+        proyecto <- proyecto$Calificación
+        tibble(Exámenes = examenes,
+                                Tareas = tareas,
+                                Proyecto = proyecto,
+                                `Calificación final` = examenes * 0.6 +
+                                    tareas * 0.2 + proyecto * 0.2) 
+    }) 
     
     output$res_glob <- renderPlot({
         grupo <- c("lunes-jueves","martes-viernes")
@@ -87,23 +121,32 @@ server <- function(input, output) {
     })
     
     output$c_final <- renderTable({
-        calif_previa <- Data() %>% 
-            group_by(Grupo, Sexo, Exp, Rubro) %>%
-            summarise(Calificación = mean(Calificación))
-        examenes <- calif_previa %>%
-            filter(Rubro == "Examen") 
-        examenes <- (examenes$Calificación * 2 +
-                         input$Ex3) / 3
-        tareas <- calif_previa %>%
-            filter(Rubro == "Tarea")
-        tareas <- tareas$Calificación
-        proyecto <- input$Proy
-        tibble(Exámenes = examenes,
-               Tareas = tareas,
-               Proyecto = proyecto,
-               `Calificación final` = examenes * 0.6 +
-                   tareas * 0.2 + proyecto * 0.2)
-        
+        # calif_previa <- Data() %>% 
+        #     group_by(Grupo, Sexo, Exp, Rubro) %>%
+        #     summarise(Calificación = mean(Calificación))
+        # examenes <- calif_previa %>%
+        #     filter(Rubro == "Examen")
+        # examenes <- examenes$Calificación
+        # # examenes <- (examenes$Calificación * 2 +
+        # #                  input$Ex3) / 3 # previo
+        # tareas <- calif_previa %>%
+        #     filter(Rubro == "Tarea")
+        # tareas <- tareas$Calificación
+        # # proyecto <- input$Proy # previo
+        # proyecto <- calif_previa %>% 
+        #     filter(Rubro == "Proyecto")
+        # proyecto <- proyecto$Calificación
+        # tibble(Exámenes = examenes,
+        #        Tareas = tareas,
+        #        Proyecto = proyecto,
+        #        `Calificación final` = examenes * 0.6 +
+        #            tareas * 0.2 + proyecto * 0.2)
+    calif_previa()    
+    })
+    
+    output$c_boleta <- renderText({
+        paste("Calificación final del semestre:", "<br>", round(c(calif_previa()$`Calificación final`))
+        )
     })
 
     output$califs <- renderTable({
