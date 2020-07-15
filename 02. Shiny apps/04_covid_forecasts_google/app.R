567d8bec2552dfe84ece0417c838636a2778eb1b
pbenavidesh <pbenavides@iteso.mx>
2020-07-15 11:58:34 -0500

    Covid-19 mobility trends forecasting app

--- 02. Shiny apps/04_covid_forecasts_google/app.R
+++ 02. Shiny apps/04_covid_forecasts_google/app.R
@@ -4,12 +4,15 @@
 
 library(shiny)
 library(shinythemes)
+library(urca)
 library(tidyverse)
 library(tsibble)
 library(feasts)
 library(fable)
 library(plotly)
 library(patchwork)
+library(lubridate)
+library(fable.prophet)
 
 # google data -------------------------------------------------------------
 
@@ -20,7 +23,7 @@ google_df <- read_csv("Global_Mobility_Report.csv",
                                            col_character()))
 
 google <- google_df %>%
-    select(-c(iso_3166_2_code:census_fips_code)) %>%
+    select(-c(sub_region_2:census_fips_code)) %>%
     pivot_longer(cols = -c(country_region_code:date)) %>%
     mutate(name = str_replace(name, 
                               pattern = 
@@ -35,14 +38,21 @@ google <- google_df %>%
     mutate(name_label = sprintf("%.2f%%", value * 100)) %>% 
     as_tsibble(index = date, key = c(country_region, 
                                      sub_region_1,
-                                     sub_region_2,
                                      name))
 
+# modelos a utilizar
+
+models <- c("SNAIVE", "ETS", "ARIMA",
+            "Prophet", "Regression","Harmonic reg.",
+            "Piecewise reg.")
 
 # user interface ----------------------------------------------------------
 
 ui <- fluidPage(theme = shinytheme("superhero"),
-    titlePanel("Pronósticos de la movilidad en tiempos de COVID-19"),
+    titlePanel(tags$div(HTML('<i class="fa fa-biohazard" style = "color:#FF7B00;"></i>
+    Pronósticos de la movilidad en tiempos de COVID-19 
+                             <i class="fa fa-head-side-cough" style = "color:#FF7B00;"></i>')),
+               windowTitle = "Pronósticos de la movilidad en tiempos de COVID-19"),
     sidebarLayout(
         sidebarPanel(
             selectInput(inputId = "pais",
@@ -55,7 +65,15 @@ ui <- fluidPage(theme = shinytheme("superhero"),
             checkboxGroupInput(inputId = "sector",
                 label = "Selecciona los sectores deseados",
                 choices = levels(google$name),
-                selected = levels(google$name)
+                selected = levels(google$name),
+                inline = TRUE
+            ),
+            
+            checkboxGroupInput(inputId = "modelos",
+              label = "Escoge los modelos a estimar",
+              choices = models,
+              selected = models[1],
+              inline = TRUE
             )
             
         ),
@@ -66,16 +84,30 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                          plotlyOutput(outputId = "time_plot")
                 ),
                 # tab 2 - Modelos ####
-                tabPanel("Modelos",
-                         
+                tabPanel("Modelos", icon = icon("space-shuttle"),
+                         wellPanel(
+                           uiOutput(outputId = "output_sector"),
+                           uiOutput(outputId = "output_radio")
+                         ),
+                         tabsetPanel(type = "pills",
+                           tabPanel("Reporte", icon = icon("galactic-republic"),
+                              verbatimTextOutput(outputId = "report")
+                           ),
+                           tabPanel("Ajuste vs. entrenamiento", icon = icon("old-republic"), 
+                              dataTableOutput("fit_accuracy")
+                           ),
+                           tabPanel("Diagnóstico de residuos", icon = icon("mandalorian"),
+                             plotOutput("resid")
+                           )
+                         )
                 ),
-                # tab 3 - Residuos ####
-                tabPanel("Diagnóstico de residuos",
-                         
-                ),
-                # tab 4 - Pronósticos ####
-                tabPanel("Pronósticos",
-                         
+                # tab 3 - Pronósticos ####
+                tabPanel("Pronósticos", icon = icon("jedi"),
+                         sliderInput(inputId = "horizonte",
+                           label = "Selecciona el horizonte de pronóstico",
+                           min = 1, max = 60, value = 14
+                         ),
+                         plotOutput("forecast", width = "100%")
                 )
             ) # navbarPage
             
@@ -105,6 +137,27 @@ server <- function(input, output, session) {
           )
   })
   
+  fit <- reactive({
+    df() %>% 
+      model(
+        SNAIVE = SNAIVE(value),
+        ETS = ETS(value),
+        ARIMA = ARIMA(value),
+        Prophet = prophet(value),
+        Regression = TSLM(value ~ trend() + season()),
+        `Harmonic reg.` = ARIMA(value ~ fourier(K = 2) + PDQ(0,0,0)),
+        `Piecewise reg.` = TSLM(value ~ trend(knots = c(ymd("2020-03-18"), 
+                                                        ymd("2020-05-03"))) + 
+                                  season())
+      )
+  })
+  
+  forecast <- reactive({
+    fit() %>% 
+      fabletools::forecast(h = input$horizonte)
+  })
+
+  # tab 1- Gráficas ####
   output$time_plot <- renderPlotly({
       p <- df() %>% 
           ggplot(aes(x = date, y = value,
@@ -124,6 +177,69 @@ server <- function(input, output, session) {
       
       ggplotly(p, tooltip = c("x", "label", "color"))
   })
+  
+  # tab 2 - Modelos ####
+  output$output_sector <- renderUI({
+    radioButtons(inputId = "radio_series",
+                 label = "La serie a reportar",
+                 choices = input$sector,
+                 selected = input$sector[1],
+                 inline = TRUE
+    )
+  })
+  
+  output$output_radio <- renderUI({
+    radioButtons(inputId = "radio_models",
+                 label = "El modelo a reportar",
+                 choices = input$modelos,
+                 selected = input$modelos[1],
+                 inline = TRUE
+    )
+  })
+  
+  output$report <- renderPrint({
+    fit() %>% 
+      select(input$radio_models) %>%
+      filter(name == input$radio_series) %>% 
+      report()
+  })
+  
+  output$fit_accuracy <- renderDataTable({
+    fit() %>% 
+      select(1:3,input$modelos) %>% 
+      accuracy()
+  })
+  
+  output$resid <- renderPlot({
+    fit() %>% 
+      select(input$radio_models) %>% 
+      filter(name == input$radio_series) %>% 
+      gg_tsresiduals() +
+      ggtitle(paste0("Diagnóstico de residuos para el modelo ", input$radio_models, 
+                     " de la serie ", input$radio_series, " en ", 
+                     input$estado, ", ", input$pais))
+  })
+  
+  # tab 3 - Pronósticos ####
+  
+  output$forecast <- renderPlot({
+    forecast() %>% 
+      filter(.model %in% input$modelos) %>% 
+      autoplot(df(), size = 1,
+               level = if (length(input$modelos) == 1) {c(80,95)} else {NULL}
+                 ) +
+      geom_hline(yintercept = 0,
+                 linetype = "dashed", 
+                 color = "firebrick") +
+      annotate("text", label = "Línea base", 
+               x = last(google$date)-5, y = 0.05, 
+               size = 3, color = "firebrick") +
+      scale_y_continuous(labels = scales::percent)+
+      labs(x = "Fecha", y = "Cambio %")
+      
+      
+  }, height = function(){300* length(input$sector)})
+  
 }
 
 shinyApp(ui, server)
