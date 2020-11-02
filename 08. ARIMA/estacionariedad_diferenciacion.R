5fc48748f16c2df18570e5b5f5794ea5d8ded245
pbenavidesh <pbenavides@iteso.mx>
2020-11-02 09:30:56 -0600

    Stationarity

--- 08. ARIMA/estacionariedad_diferenciacion.R
+++ 08. ARIMA/estacionariedad_diferenciacion.R
@@ -20,7 +20,7 @@ medicamentos <- PBS %>%
   transmute(
     `Sales ($million)` = Cost,
     `Log sales` = log(Cost),
-    Dif_estacionales = difference(log(Cost), 12),
+    Dif_estacionales = difference(log(Cost), lag = 12),
     Dif_estacionales_y_primeras = log(Cost) %>% difference(lag = 12) %>% difference(lag = 1)
   ) 
 
@@ -61,28 +61,45 @@ medicamentos %>%
 medicamentos %>% 
   features(`Log sales` %>% difference(lag = 12) %>% difference(lag = 1), unitroot_ndiffs)
 
+# aus_total_retail --------------------------------------------------------
+
 aus_total_retail <- aus_retail %>%
   summarise(Turnover = sum(Turnover))
 autoplot(aus_total_retail) + ggtitle("Serie en niveles")
 
 aus_total_retail %>% 
+  features(Turnover, unitroot_kpss)
+
+
+aus_total_retail %>% 
   autoplot(log(Turnover)) + ggtitle("Serie en logaritmos")
+
+aus_total_retail %>% 
+  features(log(Turnover), unitroot_nsdiffs)
 
 aus_total_retail %>% 
   autoplot(log(Turnover) %>% difference(lag = 12)) + 
   ggtitle("Serie log con dif. estacionales")
 
 aus_total_retail %>% 
+  features(log(Turnover) %>% difference(lag = 12), unitroot_ndiffs)
+
+aus_total_retail %>% 
   features(log(Turnover) %>% difference(lag = 12), unitroot_kpss)
+
 
 aus_total_retail %>% 
   autoplot(log(Turnover) %>% difference(lag = 12) %>% difference(lag = 1)) + 
-  ggtitle("Serie log con dif. estacionales")
+  ggtitle("Serie log con dif. estacionales y primeras dif.")
+
+aus_total_retail %>% 
+  features(log(Turnover) %>% difference(lag = 12) %>% difference(lag = 1), unitroot_kpss)
 
 
 mex <- global_economy %>% 
   filter(Country == "Mexico") 
 library(patchwork)
+
 p1 <- mex %>% 
   autoplot(GDP) + ggtitle("Serie en niveles")
 
@@ -95,10 +112,89 @@ mex %>%
 p3 <- mex %>% 
   autoplot(GDP %>% difference(lag = 1)) + ggtitle("Serie en prim. diferencias")
 
+mex %>% 
+  features(log(GDP), unitroot_ndiffs)
+
 p4 <- mex %>% 
   autoplot(log(GDP) %>% difference(lag = 1)) + ggtitle("Serie log. en prim. diferencias")
 
 (p1 | p2) / (p3 | p4)  
 
 mex %>% 
-  features(log(GDP) %>% difference(lag = 1), unitroot_ndiffs)
+  features(GDP %>% difference(lag = 1), unitroot_kpss)
+
+mex %>% 
+  features(log(GDP) %>% difference(lag = 1), unitroot_kpss)
+
+
+
+# ACF PACF ----------------------------------------------------------------
+google_stock <- gafa_stock %>%
+  filter(Symbol == "GOOG") %>%
+  mutate(day = row_number()) %>%
+  update_tsibble(index = day, regular = TRUE) %>% 
+  mutate(diff_close = difference(Close))
+
+google_2015 <- google_stock %>% filter(year(Date) == 2015)
+
+google_2015 %>% ACF(Close) %>% autoplot()
+
+google_2015 %>% PACF(Close) %>% autoplot()
+
+
+us_change %>% autoplot(Consumption) +
+  labs(x = "Year", y = "Quarterly percentage change", title = "US consumption")
+
+
+us_change %>% PACF(Consumption) %>% autoplot()
+
+
+fit <- us_change %>%
+  model(ARIMA(Consumption ~ pdq(3,0,0) + PDQ(0,0,0)))
+report(fit)
+
+
+us_change %>% ACF(Consumption) %>% autoplot()
+
+fit <- us_change %>%
+  model(ARIMA(Consumption ~ pdq(0,0,3) + PDQ(0,0,0)))
+report(fit)
+
+
+medicamentos %>% 
+  autoplot(`Sales ($million)`)
+
+# Con la función summarise_by_time
+
+library(timetk)
+
+medicamentos %>% 
+  as_tibble() %>% 
+  mutate(Month = as.Date(Month)) %>% 
+  summarise_by_time(
+    .date_var = Month,
+    .by = "year",
+    Sales = sum(`Sales ($million)`)
+  ) %>% 
+  mutate(year = year(Month)) %>% 
+  as_tsibble(index = year) %>% 
+  select(year, Sales)
+
+# con index_by y summarise
+med <- medicamentos %>% 
+  index_by(Year = year(Month)) %>% 
+  summarise(Sales = sum(`Sales ($million)`))
+
+med2 <- med %>% 
+  mutate(Sales2 = Sales *2/1.5,
+         fecha = Year)
+
+med %>% 
+  ggplot(aes(x = Year, y = Sales)) + 
+  geom_line() +
+  geom_line(data = med2, aes(x = fecha, y = Sales2))
+
+df %>% 
+  ggplot(aes(x = desempleo, y = inflacion)) +
+  geom_point()+
+  geom_smooth()
